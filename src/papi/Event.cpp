0f9c1f376a7ebea9125c38308cca9a80d4a67430
musergi <sergi.mus@gmail.com>
2022-02-20 15:33:56 +0100

    Changed build structure

--- src/papi/Event.cpp
+++ src/papi/Event.cpp
@@ -1,16 +1,44 @@
 #include "Event.hpp"
 
+#include <cstring>
 #include <stdexcept>
-#include <papi.h>
 
 Event::Event(unsigned int code) : code(code) {
 }
 
 std::string Event::getSymbol() {
   PAPI_event_info_t eventInfo;
-  int error = PAPI_get_event_info(event_code, &eventInfo);
+  getEventInfo(&eventInfo);
+  return eventInfo.symbol;
+}
+
+std::string Event::getShortDescription() {
+  PAPI_event_info_t eventInfo;
+  getEventInfo(&eventInfo);
+  return eventInfo.short_descr;
+}
+
+std::string Event::getLongDescription() {
+  PAPI_event_info_t eventInfo;
+  getEventInfo(&eventInfo);
+  return eventInfo.long_descr;
+}
+
+std::string Event::getUnits() {
+  PAPI_event_info_t eventInfo;
+  getEventInfo(&eventInfo);
+  return eventInfo.units;
+}
+
+bool Event::isDerived() {
+  PAPI_event_info_t eventInfo;
+  getEventInfo(&eventInfo);
+  return strcmp(eventInfo.derived, "NOT_DERIVED") != 0;
+}
+
+void Event::getEventInfo(PAPI_event_info_t *eventInfo) {
+  int error = PAPI_get_event_info(code, eventInfo);
   if (error != PAPI_OK) {
     throw std::logic_error("Failed to fetch event info");
   }
-  return eventInfo.symbol;
 }
--- src/papi/Event.hpp
+++ src/papi/Event.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
 #include <string>
+#include <papi.h>
 
 class Event {
+private:
   unsigned int code;
 public:
   Event(unsigned int code);
   std::string getSymbol();
+  std::string getShortDescription();
+  std::string getLongDescription();
+  std::string getUnits();
+  bool isDerived();
+private:
+  void getEventInfo(PAPI_event_info_t *eventInfo);
 };
