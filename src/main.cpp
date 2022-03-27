8598cb59b0ecd3b8ec745241aebbfe0bde306cc1
Sergi Mus <sergi.mus@gmail.com>
2022-03-27 19:49:53 +0200

    Added event listing

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include "ArgumentParser.hpp"
+#include "Event.hpp"
 
 int main(int argc, const char **argv) {
   ArgumentParser parser;
   parser.addArgument("list");
   parser.parseArgs(argc, argv);
   if (parser.isSet("list")) {
-    std::cout << "Entering listing mode" << std::endl;
+    std::cout << "Entering listing mode." << std::endl;
+    Event event = Event::first();
+    std::cout << event.getSymbol();
+    while (event.hasNext()) {
+      event = event.next();
+      std::cout << "," << event.getSymbol();
+    }
+    std::cout << std::endl;
   }
   return 0;
 }
