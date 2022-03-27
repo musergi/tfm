ae12f0048036bb068e034e09d90e594c239a483d
Sergi Mus <sergi.mus@gmail.com>
2022-03-27 19:30:01 +0200

    Changed argument type

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "ArgumentParser.hpp"
 
-int main(int argc, char **argv) {
+int main(int argc, const char **argv) {
   ArgumentParser parser;
   parser.addArgument("list");
   parser.parseArgs(argc, argv);
