64e5f72b8c64edf67658ab47f408f55502f04efd
Sergi Mus <sergi.mus@gmail.com>
2022-03-28 18:04:02 +0200

    Added namespace again

--- src/papi/PapiException.cpp
+++ src/papi/PapiException.cpp
@@ -8,7 +8,7 @@ PapiException::PapiException(int error) : std::runtime_error(PapiException::getP
 
 std::string PapiException::getPapiError(int code) {
     std::string message("PAPI error:");
-    message += to_string(code);
+    message += std::to_string(code);
     message += ":";
     message += PAPI_strerror(code);
     return message;
