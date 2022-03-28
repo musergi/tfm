6832bbc3920e50488ec018116d7d1e5d398a51ce
Sergi Mus <sergi.mus@gmail.com>
2022-03-28 18:07:33 +0200

    Changed exception to use stringstream

--- src/papi/PapiException.cpp
+++ src/papi/PapiException.cpp
@@ -1,15 +1,16 @@
 #include "PapiException.hpp"
 
-#include <string>
+#include <sstream>
 #include <papi.h>
 
 PapiException::PapiException(int error) : std::runtime_error(PapiException::getPapiError(error)) {
 }
 
 std::string PapiException::getPapiError(int code) {
-    std::string message("PAPI error:");
-    message += std::to_string(code);
-    message += ":";
-    message += PAPI_strerror(code);
-    return message;
+    std::ostringstream message;
+    message << "PAPI error:";
+    message << code;
+    message << ":";
+    message << PAPI_strerror(code);
+    return message.str();
 }
