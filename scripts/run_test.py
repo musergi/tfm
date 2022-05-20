6eeb71ffbc635fb2a35ddf23a22725fc7392de36
musergi <sergi.mus@gmail.com>
2022-05-20 17:29:51 +0200

    Added auto labeling

--- scripts/run_test.py
+++ scripts/run_test.py
@@ -2,6 +2,9 @@ import os
 import subprocess
 import argparse
 import time
+from datetime import datetime
+
+from requests import head
 
 if os.geteuid() != 0:
     exit("Root privileges are required to run this script")
@@ -13,6 +16,18 @@ args = parser.parse_args()
 
 recording_proc = subprocess.Popen(args=['./build/src/record', *args.events])
 time.sleep(1)
+start_time = round(datetime.now().timestamp() * 1000)
 exploit_proc = subprocess.Popen(args=['taskset', '0x1', args.vuln])
 exploit_proc.wait()
+end_time = round(datetime.now().timestamp() * 1000)
 recording_proc.wait()
+
+with open('out.csv') as f:
+    data = map(lambda l: l.split(','), f)
+    headers = next(data)
+    timestamp_index = headers.index('timestamp')
+    headers.append('label')
+    new_content = map(lambda row: [*row, 1] if start_time < row[timestamp_index] < end_time else [*row, 0], data)
+    new_lines = [headers] + list(new_content)
+    with open('out_labeled.csv', 'w') as f:
+        f.writelines(new_lines)
