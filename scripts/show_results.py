4ae8997390b39659fb3c41554b0a92e0f0d8bfd4
musergi <sergi.mus@gmail.com>
2022-05-21 17:07:52 +0200

    Added new visualization

--- scripts/show_results.py
+++ scripts/show_results.py
@@ -1,6 +1,19 @@
+import argparse
+from string import printable
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('out.csv')
-df.plot(y='PAPI_L1_TCM', use_index=True)
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', required=True)
+args = parser.parse_args()
+
+df = pd.read_csv(args.dataset)
+df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+print(df.head())
+printable_cols = list(df.columns)
+printable_cols.remove('timestamp')
+fig, axs = plt.subplots(len(printable_cols))
+for ax, col in zip(axs, printable_cols):
+    ax.plot(df['timestamp'], df[col])
+    ax.set_title(col)
 plt.show()
