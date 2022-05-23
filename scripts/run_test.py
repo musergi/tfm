03b1606bdb54d1e56bc331aae8087f60e063ccce
musergi <sergi.mus@gmail.com>
2022-05-23 18:43:26 +0200

    Added stress proc

--- scripts/run_test.py
+++ scripts/run_test.py
@@ -6,22 +6,30 @@ from datetime import datetime
 
 from requests import head
 
+# Assert that the user is running as root
 if os.geteuid() != 0:
     exit("Root privileges are required to run this script")
 
+# Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--vuln', required=True)
 parser.add_argument('--events', required=True, nargs='+')
+parser.add_argument('--stress')
 args = parser.parse_args()
 
+# Run the processes
 recording_proc = subprocess.Popen(args=['./build/src/record', *args.events])
-time.sleep(1)
+time.sleep(0.5)
+stress_proc = subprocess.Popen(args=['taskset', '0x1', 'stress'])
+time.sleep(0.5)
 start_time = round(datetime.now().timestamp() * 1000)
 exploit_proc = subprocess.Popen(args=['taskset', '0x1', args.vuln])
 exploit_proc.wait()
 end_time = round(datetime.now().timestamp() * 1000)
 recording_proc.wait()
+stress_proc.kill()
 
+# Dataset labeling
 with open('out.csv') as f:
     data = list(map(lambda l: l.strip().split(','), f))
     headers = data[0]
