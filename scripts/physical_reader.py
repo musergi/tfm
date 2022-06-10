1312a0139f9cde631e9b90f1a6f8f6f57b467591
musergi <sergi.mus@gmail.com>
2022-06-10 09:19:52 +0200

    Added physical reader memdump

--- scripts/physical_reader.py
+++ scripts/physical_reader.py
@@ -2,6 +2,6 @@
 import time
 import subprocess
 
-p = subprocess.Popen(['./deps/meltdown/secret', '0x240000000'])
+p = subprocess.Popen(['./deps/meltdown/memdump', '0x240000000'])
 time.sleep(2)
 p.kill()
