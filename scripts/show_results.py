1cb462ded3dc195288b6104469bcb5dac7d560d6
musergi <sergi.mus@gmail.com>
2022-06-10 11:32:01 +0200

    Modified plt script

--- scripts/show_results.py
+++ scripts/show_results.py
@@ -5,15 +5,25 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', required=True)
+parser.add_argument('--column', required=True)
 args = parser.parse_args()
 
 df = pd.read_csv(args.dataset)
 df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 print(df.head())
-printable_cols = list(df.columns)
-printable_cols.remove('timestamp')
-fig, axs = plt.subplots(len(printable_cols))
-for ax, col in zip(axs, printable_cols):
-    ax.plot(df['timestamp'], df[col])
-    ax.set_title(col)
-plt.show()
+if args.column is None:
+    printable_cols = list(df.columns)
+    printable_cols.remove('timestamp')
+    fig, axs = plt.subplots(len(printable_cols))
+    for ax, col in zip(axs, printable_cols):
+        ax.plot(df['timestamp'], df[col])
+        ax.set_title(col)
+    plt.show()
+else:
+    plt.plot(df['timestamp'], df[args.column])
+    plt.xlabel('Timestamp')
+    if args.column.islower():
+        plt.ylabel(args.column.capitalize())
+    else:
+        plt.ylabel(args.column)
+    plt.show()
